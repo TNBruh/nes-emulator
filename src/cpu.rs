600a02fd5297ea8b09d2782d0fbf3edeaab82fda
peppino <nicetry@fed.pog>
2023-12-07 03:34:20 +0700

    spending my waking moment on something that no one gonna appreciate

--- src/cpu.rs
+++ src/cpu.rs
@@ -27,14 +27,36 @@ impl CPU {
         self.memory[addr as usize] = data
     }
 
+    fn mem_read_u16(&self, addr: u16) -> u16 {
+        u16::from_be_bytes([
+            self.mem_read(addr+1),
+            self.mem_read(addr)
+        ])
+    }
+
+    fn mem_write_u16(&mut self, addr: u16, data: u16) {
+        let data_bytes = data.to_be_bytes();
+        self.mem_write(addr, data_bytes[1]);
+        self.mem_write(addr+1, data_bytes[0])
+    }
+
+    pub fn reset(&mut self) {
+        self.register_a = 0;
+        self.register_x = 0;
+        self.status = 0;
+
+        self.program_counter = self.mem_read_u16(0xFFFC);
+    }
+
     pub fn load(&mut self, program: Vec<u8>) {
         self.memory[0x8000 .. (0x8000 + program.len())].copy_from_slice(&program);
-        self.program_counter = 0x8000;
+        self.mem_write_u16(0xFFFC, 0x8000);
     }
 
     pub fn load_and_run(&mut self, program: Vec<u8>) {
         self.load(program);
-        self.run()
+        self.reset();
+        self.run();
     }
 
     pub fn run(&mut self) {
--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,58 @@ pub mod cpu;
 
 fn main() {
     let num: u16 = 0x1234;
-    println!("Hello, world!");
+    let le_num = num.to_le_bytes();
+
+    println!("{:X} {:X}", le_num[0], le_num[1]);
+    println!("{}", u16::from_be_bytes([0x01, 0x00]));
 }
 
+#[cfg(test)]
+mod test {
+    use crate::cpu::CPU;
+
+    #[test]
+    fn test_0xa9_lda_immediate_load_data() {
+        let mut cpu = CPU::new();
+        cpu.load_and_run(vec![0xa9, 0x05, 0x00]);
+        assert_eq!(cpu.register_a, 0x05);
+        assert!(cpu.status & 0b0000_0010 == 0b00);
+        assert!(cpu.status & 0b1000_0000 == 0);
+    }
+
+    #[test]
+    fn test_0xa9_lda_zero_flag() {
+        let mut cpu = CPU::new();
+        cpu.load_and_run(vec![0xa9, 0x00, 0x00]);
+        assert!(cpu.status & 0b0000_0010 == 0b10);
+    }
+
+    #[test]
+    fn test_0xaa_tax_move_a_to_x() {
+        let mut cpu = CPU::new();
+        cpu.register_a = 10;
+        cpu.load_and_run(vec![0xaa, 0x00]);
+
+        assert_eq!(cpu.register_x, 10)
+    }
+
+    #[test]
+    fn test_5_ops_working_together() {
+        let mut cpu = CPU::new();
+        cpu.load_and_run(vec![0xa9, 0xc0, 0xaa, 0xe8, 0x00]);
+
+        assert_eq!(cpu.register_x, 0xc1)
+    }
+
+    #[test]
+    fn test_inx_overflow() {
+        let mut cpu = CPU::new();
+        cpu.register_x = 0xff;
+        cpu.load_and_run(vec![0xe8, 0xe8, 0x00]);
+
+        assert_eq!(cpu.register_x, 1)
+    }    
+}
 
 // #[cfg(test)]
 // mod test {
